aa150b3e2a42e08ef6d210855db07bd848299239
Martin6232 <martinrodriguez@liceovvh.cl>
2025-03-11 10:52:47 -0300

    ok

--- JS/03-ejer/script.js
+++ JS/03-ejer/script.js
@@ -1,6 +1,16 @@
 let numero = 14;
 let string = "holo";
 let bool = true;
+let multiple = [25, 34, 8, 3];
+const datos = {
+    especie: "gato",
+    nombre: "morice"
+};
+let vacio = undefined;
+let nulo = null;
+
+let matriz = [[1, 2], [3, 4]];
+
 
 function int() {
     alert(numero)
@@ -11,5 +21,28 @@ function text() {
 };
 
 function pregunta() {
-    alert(bool)
+    alert("¿Las uvas verdes son más ricas? = " + bool)
+};
+
+function array() {
+    alert(multiple)
+};
+
+function indef() {
+    alert(vacio)
+};
+
+function matrix() {
+    alert("matriz:\n" + matriz)
+};
+
+function nada() {
+    alert(nulo)
+};
+
+function suma(A, B, C) {
+    A = 20,
+    B = 80,
+    C = A + B,
+    alert(A + " + " + B + " = " + C)
 };
