810d2a09af54e63ac69ac4b479107715d9d12898
martin6232 <martinrodriguez@liceovvh.cl>
2025-03-17 20:06:27 -0300

    ok

--- ejer_03/script.js
+++ ejer_03/script.js
@@ -2,7 +2,12 @@ let datos = [1,2,3,4,5,6,7];
 
 function Suma_indices() {
     let sumar = datos[0] + datos[5];
-    alert("la suma de los datos es: " + sumar);
+    alert("La suma de los datos es: " + sumar);
 }
 
 let pares = [2,4,6,8,10,12]
+
+function Producto_pares() {
+    let par = pares[0] * pares[1] * pares[2] * pares[3] * pares[4] * pares[5];
+    alert("La multiplicacion de los datos es: " + par)
+}
