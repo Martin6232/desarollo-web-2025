4b1ac5dd127c34e4fdd924fd58c38f4b1d15d116
Martin6232 <martinrodriguez@liceovvh.cl>
2025-03-19 08:40:42 -0300

    ok

--- ejer_03/script.js
+++ ejer_03/script.js
@@ -1,11 +1,11 @@
 function Suma_indices() {
     let datos = [1,2,3,4,5,6,7];
-    let sumar = datos[0] + datos[5];
-    alert("La suma de los datos es: " + sumar);
+    let sumar = datos[0] + datos[1] + datos[2] + datos[3] + datos[4] + datos[5] + datos[6];
+    alert(`El arreglo es: ${datos.join(" - ")} \nResultado: ${sumar}`);
 }
 
 function Producto_pares() {
     let pares = [2,4,6,8,10,12];
     let par = pares[0] * pares[1] * pares[2] * pares[3] * pares[4] * pares[5];
-    alert("La multiplicacion de los datos es: " + par);
+    alert(`La multiplicacion de los datos es: ${pares.join(" - ")} \nResultado: ${par}` );
 }
