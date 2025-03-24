04452b96f0862ed38709b9564831a5c4c361db9c
martin6232 <martinrodriguez@liceovvh.cl>
2025-03-23 22:25:22 -0300

    ok

--- ejer_04_05/script.js
+++ ejer_04_05/script.js
@@ -55,21 +55,47 @@ function puntuacion() {
 }
 //5
 function verificar() {
-    let edad = parseInt(prompt("Ingresa tu edad"))
-    let condiciones = prompt("Aceptas las condiciones")
+    let edad = parseInt(prompt("Ingresa tu edad"));
+    let condiciones = prompt("Aceptas las condiciones");
     if(edad >= 18 && condiciones == "si") {
-        alert("Acceso permitido")
+        alert("Acceso permitido");
     } else {
-        alert("Acceso denegado")
+        alert("Acceso denegado");
     }
 }
 
 function descuento() {
-    let edad = parseInt(prompt("Ingresa tu edad"))
-    let quest = prompt("Eres estudiante")
+    let edad = parseInt(prompt("Ingresa tu edad"));
+    let quest = prompt("Eres estudiante");
     if(edad > 60 || quest == "si") {
-        alert("Descuento aplicado")
+        alert("Descuento aplicado");
     } else {
-        alert("No tienes descuento")
+        alert("No tienes descuento");
     }
+}
+
+function validacion() {
+    let usuario = prompt("Registra tu nombre de usuario: ");
+    let contraseña = prompt("Registra tu contraseña: ");
+    if(!usuario || !contraseña){
+        alert("Error: Debes ingresar usuario y contraseña");
+        return;
+    } else {
+        alert("Inicio de sesión exitoso");
+        return;
+    }
+}
+
+function encendido() {
+    let energiaSuficiente = parseInt(prompt("¿Cuanta bateria le queda a la maquina?"));
+    let interruptorEncendido = prompt("¿El interuptor esta encendido?");
+    if (energiaSuficiente >= 15 && interruptorEncendido == "si") {
+        alert("Máquina encendida");
+    } else {
+        alert("No se puede encender la máquina")
+    }
+}
+
+function accesoRestringido() {
+    
 }
