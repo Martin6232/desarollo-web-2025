4b64acae08ba19dbeebb57f941233936f020b56c
Martin6232 <martinrodriguez@liceovvh.cl>
2025-04-01 10:42:13 -0300

    ok

--- ejer_08/script.js
+++ ejer_08/script.js
@@ -13,5 +13,24 @@ function multiplicar() {
 
 function ingresar() {
     let n = parseInt(prompt("Ingresa tu numero: "));
+    let last = 0;
+    for(let num = 1; num <= n; num++){
+        last += num
+        if(num >= n) {
+            alert(`El resultado es: ${last}`);
+        }
+    }
+}
 
+function pares() {
+    let num = parseInt(prompt("Ingrese su primer numero: "));
+    let num2 = parseInt(prompt("Ingrese su ultimo numero: "));
+    let array = []
+    for(let n = 0;num <= num2; num++){
+        if (num % 2 == 0) {
+            n = num
+            array.push(n);
+            alert(array);
+        }
+    }
 }
