e7d2ab237540ba5b5d7a552cf563c1eeb5e1d02b
martin6232 <martinrodriguez@liceovvh.cl>
2025-07-09 09:28:17 -0400

    ok

--- JS/ejer_2.0/script.js
+++ JS/ejer_2.0/script.js
@@ -10,3 +10,35 @@ function comprasdelpop() {
     carrito.pop()
     document.getElementById("resultado").innerHTML = `Carro: <br>-${carrito.join("<br>-")}`;
 }
+/* 3 y 4 */
+let colores = ["Rojo","Azul"]
+
+document.getElementById("list").innerHTML = `Colores: ${colores.join(" - ")}`
+function colors() {
+    let elemento = document.getElementById("list-color").value;
+    let color = elemento.trim()
+    if(color === "") return;
+    colores.unshift(color)
+    document.getElementById("list").innerHTML = `Colores: ${colores.join(" - ")}`
+}
+
+function delcolor() {
+    colores.shift()
+    document.getElementById("list").innerHTML = `Colores: ${colores.join(" - ")}`
+}
+/* ejer 5 */
+let numeros = [10, 20, 30]
+document.getElementById("list-num").innerHTML = `${numeros.join(" - ")}`
+
+function num() {
+    let elemento = document.getElementById("add-num").value;
+    let num = elemento.trim()
+    if (num === "") return;
+    numeros.unshift(num)
+    document.getElementById("list-num").innerHTML = `${numeros.join(" - ")}`
+}
+
+function delnum() {
+    numeros.shift()
+    document.getElementById("list-num").innerHTML = `${numeros.join(" - ")}`
+}
