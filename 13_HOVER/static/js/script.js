ef030a8f3895ba32a1fc64295d5ce5fe3dc97d10
martin6232 <martinrodriguez@liceovvh.cl>
2025-10-09 11:09:14 -0300

    ok

--- 13_HOVER/static/js/script.js
+++ 13_HOVER/static/js/script.js
@@ -3,10 +3,61 @@ function hover1(element) {
     let span = element.querySelector('span');
     img.remove();
     span.remove();
-    document.getElementById('cont').innerHTML = '<video width="150" autoplay src="resources/videos/PREMEDITATED (Full Combo) - FNF Myths of Yamaha.mp4"></video>';
+    document.getElementById('cont').innerHTML = '<video width="150" height="115" controls autoplay src="resources/videos/PREMEDITATED (Full Combo) - FNF Myths of Yamaha.mp4" class="vid-s"></video> <span class="video-title">PREMEDITATED: Myths of Yamaha</span>';
 }
 
 function out1(element) {
+    let video = element.querySelector('video');
+    let span = element.querySelector('span');
+    video.remove();
+    span.remove();
+    document.getElementById('cont').innerHTML = '<img src="resources/img/ZWzXbVEC8fU_1280x720.jpg" class="vid-s" id="video"> <span class="video-title">PREMEDITATED: Myths of Yamaha</span>';
+}
+
+function hover2(element) {
     let img = element.querySelector('img');
-    img.appendchild = '<img src="resources/img/ZWzXbVEC8fU_1280x720.jpg" class="vid-s" id="video">';
+    let span = element.querySelector('span');
+    img.remove();
+    span.remove();
+    document.getElementById('cont').innerHTML = '<video width="150" height="115" controls autoplay src="resources/videos/PREMEDITATED (Full Combo) - FNF Myths of Yamaha.mp4" class="vid-s"></video> <span class="video-title">PREMEDITATED: Myths of Yamaha</span>';
+}
+
+function out2(element) {
+    let video = element.querySelector('video');
+    let span = element.querySelector('span');
+    video.remove();
+    span.remove();
+    document.getElementById('cont').innerHTML = '<img src="resources/img/ZWzXbVEC8fU_1280x720.jpg" class="vid-s" id="video"> <span class="video-title">PREMEDITATED: Myths of Yamaha</span>';
+}
+
+function hover3(element) {
+    let img = element.querySelector('img');
+    let span = element.querySelector('span');
+    img.remove();
+    span.remove();
+    document.getElementById('cont').innerHTML = '<video width="150" height="115" controls autoplay src="resources/videos/PREMEDITATED (Full Combo) - FNF Myths of Yamaha.mp4" class="vid-s"></video> <span class="video-title">PREMEDITATED: Myths of Yamaha</span>';
+}
+
+function out3(element) {
+    let video = element.querySelector('video');
+    let span = element.querySelector('span');
+    video.remove();
+    span.remove();
+    document.getElementById('cont').innerHTML = '<img src="resources/img/ZWzXbVEC8fU_1280x720.jpg" class="vid-s" id="video"> <span class="video-title">PREMEDITATED: Myths of Yamaha</span>';
+}
+
+function hover4(element) {
+    let img = element.querySelector('img');
+    let span = element.querySelector('span');
+    img.remove();
+    span.remove();
+    document.getElementById('cont').innerHTML = '<video width="150" height="115" controls autoplay src="resources/videos/PREMEDITATED (Full Combo) - FNF Myths of Yamaha.mp4" class="vid-s"></video> <span class="video-title">PREMEDITATED: Myths of Yamaha</span>';
+}
+
+function out4(element) {
+    let video = element.querySelector('video');
+    let span = element.querySelector('span');
+    video.remove();
+    span.remove();
+    document.getElementById('cont').innerHTML = '<img src="resources/img/ZWzXbVEC8fU_1280x720.jpg" class="vid-s" id="video"> <span class="video-title">PREMEDITATED: Myths of Yamaha</span>';
 }
